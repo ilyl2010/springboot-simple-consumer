a74ee0bb4442089dd8bc5b953c631ff95466a2dd
zengzhen <zengzhen@si-tech.com.cn>
2019-07-10 09:28:33 +0800

    first commit

---
+++ src/main/java/com/zc/springbootsimpleconsumer/SpringbootSimpleConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zc.springbootsimpleconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zc/springbootsimpleconsumer/SpringbootSimpleConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zc.springbootsimpleconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSimpleConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
