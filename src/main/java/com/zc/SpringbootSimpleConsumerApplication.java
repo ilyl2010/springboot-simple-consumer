d282cd297e302ecaf66c1310e6bfd990289aacd8
zengzhen <zengzhen@si-tech.com.cn>
2019-07-10 09:51:06 +0800

    first commit

--- src/main/java/com/zc/SpringbootSimpleConsumerApplication.java
+++ src/main/java/com/zc/SpringbootSimpleConsumerApplication.java
@@ -1,10 +1,16 @@
-package com.zc.springbootsimpleconsumer;
+package com.zc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootSimpleConsumerApplication {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSimpleConsumerApplication.class, args);
